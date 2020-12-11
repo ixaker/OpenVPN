8019490e0f38c035c3c71f492656fe881eea4536
ixaker <xaker.dnepr@gmail.com>
2020-12-11 15:24:06 +0200

    Update install_openvpn.sh

--- install_openvpn.sh
+++ install_openvpn.sh
@@ -273,12 +273,12 @@ client-to-client
 mode server
 server 10.8.0.0 255.255.255.0" > /etc/openvpn/server/server.conf
 	# IPv6
-	if [[ -z "$ip6" ]]; then
+	# if [[ -z "$ip6" ]]; then
 		# echo 'push "redirect-gateway def1 bypass-dhcp"' >> /etc/openvpn/server/server.conf
-	else
+	# else
 		# echo 'server-ipv6 fddd:1194:1194:1194::/64' >> /etc/openvpn/server/server.conf
 		# echo 'push "redirect-gateway def1 ipv6 bypass-dhcp"' >> /etc/openvpn/server/server.conf
-	fi
+	# fi
 	echo 'ifconfig-pool-persist ipp.txt' >> /etc/openvpn/server/server.conf
 	# DNS
 	case "$dns" in
